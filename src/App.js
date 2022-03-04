d789e64051aea333692455c60ace4ec62f5508ec
JSIJSI <jynj94@naver.com>
2022-03-05 00:33:26 +0900

    0305 commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Home from "./pages/Home";
+import New from "./pages/New";
+import Edit from "./pages/Edit";
+import Diary from "./pages/Diary";
+
+import RouteTest from "./components/RouteTest";
 
 function App() {
-  return null;
+  return (
+    <BrowserRouter>
+      {/* 페이지 어디에서든지 보이는 요소가 필요하면 Routes 바깥으로 위치시키면 된다. */}
+      <h2 className="App">App.js</h2>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/New" element={<New />} />
+        <Route path="/Edit" element={<Edit />} />
+        <Route path="/Diary" element={<Diary />} />
+      </Routes>
+      <RouteTest></RouteTest>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/pages/Diary.js
@@ -0,0 +1,12 @@
+//컴포넌트 생성 단축키 rsc 기억!!
+import React from "react";
+
+const Diary = () => {
+  return (
+    <div>
+      <h1>Diary</h1>
+    </div>
+  );
+};
+
+export default Diary;
